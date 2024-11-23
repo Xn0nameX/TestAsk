9267d438f2b0d659a93e254bd5b1a96cf7c3b5d4
Xn0nameX <dana_55348188@mail.ru>
2024-11-23 14:56:06 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace TestAsk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.ButtonHelp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(561, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(776, 397);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // ButtonHelp
+            // 
+            this.ButtonHelp.Location = new System.Drawing.Point(432, 415);
+            this.ButtonHelp.Name = "ButtonHelp";
+            this.ButtonHelp.Size = new System.Drawing.Size(123, 23);
+            this.ButtonHelp.TabIndex = 2;
+            this.ButtonHelp.Text = "Помощь";
+            this.ButtonHelp.UseVisualStyleBackColor = true;
+            this.ButtonHelp.Click += new System.EventHandler(this.ButtonHelp_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ButtonHelp);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button ButtonHelp;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using TestAsk.Properties;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TestAsk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Dictionary<string, string> nodeValues = new Dictionary<string, string>();
+
+        private string GetNodeValue(TreeNode node, string filePath)
+        {
+            string value = string.Empty;
+
+            using (XmlReader reader = XmlReader.Create(filePath))
+            {
+                while (reader.Read())
+                {
+                    if (reader.NodeType == XmlNodeType.Element && reader.Name == node.Text.Split('[')[0])
+                    {
+                        if (reader.Read() && reader.NodeType == XmlNodeType.Text)
+                        {
+                            value = reader.Value;
+                        }
+                        break;
+                    }
+                }
+            }
+
+            return value;
+        }
+
+        private async void LoadXmlDoc(string filePath)
+        {
+            treeView1.Nodes.Clear();
+            nodeValues.Clear();
+
+            XmlReaderSettings settings = new XmlReaderSettings
+            {
+                Async = true
+            };
+
+            using (XmlReader reader = XmlReader.Create(filePath, settings))
+            {
+                while (await reader.ReadAsync())
+                {
+                    if (reader.NodeType == XmlNodeType.Element)
+                    {
+                        TreeNode node = new TreeNode(reader.Name);
+                        await Task.Run(() => AddChildNodes(reader, node));
+                        treeView1.Invoke((MethodInvoker)delegate {
+                            treeView1.Nodes.Add(node);
+                        });
+                    }
+                }
+            }
+        }
+
+
+
+        private void AddChildNodes(XmlReader reader, TreeNode treeNode)
+        {
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.Push(treeNode);
+
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+
+                    TreeNode childNode = new TreeNode(reader.Name);
+                    stack.Peek().Nodes.Add(childNode);
+                    stack.Push(childNode);
+
+                    
+                    if (!reader.IsEmptyElement && reader.Read() && reader.NodeType == XmlNodeType.Text)
+                    {
+                        nodeValues[childNode.Text] = reader.Value;
+                        childNode.Text += $": {reader.Value}";
+                    }
+                }
+                else if (reader.NodeType == XmlNodeType.EndElement)
+                {
+                    stack.Pop();
+                    if (stack.Count == 0) break;
+                }
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            string filePath = openFileDialog1.FileName;
+            LoadXmlDoc(filePath);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            openFileDialog1.Filter = "Xml files (*.xml)|*.xml";
+            openFileDialog1.Title = "Выберите xml файл";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog1.FileName;
+            }
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+     
+        }
+    
+        private void ButtonHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("\nАвтор: Метелев Даниил Андреевич\nДата выполнения: 24.11.2024", "Помощь");
+        }
+    }
+}
